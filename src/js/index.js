17109a42b963cc31717404e496b0248090cbadc8
Airan <airanschez0@gmail.com>
2020-10-06 21:12:04 +0100

    added new features

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,17 @@
 const dice = document.getElementById('dice');
 const button = document.getElementById('button');
+const record = document.getElementById('list')
+let result;
 
 button.addEventListener('click', () => {
   var throwValue = Math.floor(Math.random() * 6) + 1;
-  console.log(throwValue);
-  dice.innerText = throwValue;
+  dice.innerHTML = throwValue
+  createRecord(throwValue)
 });
+
+function createRecord(throwValue) {
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode("Has lanzado: " + throwValue);
+  node.appendChild(textnode);
+  record.appendChild(node);
+}
